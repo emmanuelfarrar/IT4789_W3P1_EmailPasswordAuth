3f1e4a39081199d250bc364e32c8cfae2adc09fe
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-05-22 02:48:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/omora/w3p1_emailpasswordauth/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.omora.w3p1_emailpasswordauth;
+/**
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+*/
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "EmailPassword";
+    private EditText editText;
+    private EditText editText2;
+    private Button button;
+    private Button button2;
+
+    private String email;
+    private String password;
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                email = editText.getText().toString();
+                password = editText2.getText().toString();
+
+                signIn(email, password);
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = editText.getText().toString();
+                password = editText2.getText().toString();
+
+                createAccount(email, password);
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:not implemented yet:" + email);
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = editText.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            editText.setError("Required.");
+            valid = false;
+        } else {
+            editText.setError(null);
+        }
+
+        String password = editText2.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            editText2.setError("Required.");
+            valid = false;
+        } else {
+            editText2.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+}
+
